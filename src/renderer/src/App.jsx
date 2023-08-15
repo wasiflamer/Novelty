36bdcc9a5c6fd7cd1018493086e9ec69051969c9
wasiflamer <wasiflamer@gmail.com>
2023-08-15 20:04:57 +0500

    drawer overlap

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -17,13 +17,34 @@ function Example() {
   return (
     <>
       <Navbar />
+
       <div class="grid grid-cols-4 gap-4 text-center m-8 disabledrag">
+        <div className="card w-96 bg-base-100 shadow-xl image-full">
+          <figure>
+            <img
+              src="https://images.unsplash.com/photo-1691719183611-705430ca9191?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+              alt="Shoes"
+              height={30}
+              width={30}
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+
         <div>
           <div className="card w-96 bg-base-100 shadow-xl image-full">
             <figure>
               <img
                 src="https://images.unsplash.com/photo-1691719183611-705430ca9191?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
                 alt="Shoes"
+                height={30}
+                width={30}
               />
             </figure>
             <div className="card-body">
@@ -35,13 +56,9 @@ function Example() {
             </div>
           </div>
         </div>
-        <div>2</div>
+
         <div>3</div>
         <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>8</div>
       </div>
     </>
   )
