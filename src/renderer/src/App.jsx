ec5d0a7aa3358fe9d682c6c89127ae346964c68f
wasiflamer <wasiflamer@gmail.com>
2023-08-15 20:01:58 +0500

    scroll bar added

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -17,17 +17,20 @@ function Example() {
   return (
     <>
       <Navbar />
-      <div class="grid grid-cols-4 gap-4 text-center m-8">
+      <div class="grid grid-cols-4 gap-4 text-center m-8 disabledrag">
         <div>
-          <div className="card lg:card-side bg-base-100 shadow-xl">
+          <div className="card w-96 bg-base-100 shadow-xl image-full">
             <figure>
-              <img src="/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" />
+              <img
+                src="https://images.unsplash.com/photo-1691719183611-705430ca9191?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+                alt="Shoes"
+              />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">New album is released!</h2>
-              <p>Click the button to listen on Spotiwhy app.</p>
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
               <div className="card-actions justify-end">
-                <button className="btn btn-primary">Listen</button>
+                <button className="btn btn-primary">Buy Now</button>
               </div>
             </div>
           </div>
